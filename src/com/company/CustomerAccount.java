88e6e3f148e017bc30f22e295296bdc39aac09e7
is147tix <43656158+is147tix@users.noreply.github.com>
2018-11-29 19:43:37 -0500

    Add files via upload

--- src/com/company/CustomerAccount.java
+++ src/com/company/CustomerAccount.java
@@ -1,24 +1,35 @@
+
+
+package com.company;
+
 import java.util.Scanner;
 
 public class CustomerAccount {
 
     static Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args) {
 
+    public String custInfo() {
         System.out.print("Please enter your first name: ");
         String enteredFirst = input.nextLine();
-        String firstName = enteredFirst.substring(0,1).toUpperCase() + enteredFirst.substring(1).toLowerCase();
+        String firstName = enteredFirst.substring(0, 1).toUpperCase() + enteredFirst.substring(1).toLowerCase();
 
         System.out.print("Please enter your last name: ");
         String enteredLast = input.nextLine();
-        String lastName = enteredLast.substring(0,1).toUpperCase() + enteredLast.substring(1).toLowerCase();
+        String lastName = enteredLast.substring(0, 1).toUpperCase() + enteredLast.substring(1).toLowerCase();
 
-        int confirmationNum =  (int)(Math.random() * 9999 + 1);
+        int confirmationNum = (int) (Math.random() * 9999 + 1);
 
         System.out.println(lastName + ", " + firstName);
 
-        System.out.println("Confirmation # " + confirmationNum);
+        System.out.println("Account Number # " + confirmationNum);
 
+        return lastName;
+    }
+
+    public int age() {
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+        return age;
     }
 }
