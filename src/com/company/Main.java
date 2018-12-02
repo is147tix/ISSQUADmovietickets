3ec0f492a99746efb36cea48bfb3e6e41bc92e33
is147tix <43656158+is147tix@users.noreply.github.com>
2018-12-01 23:00:10 -0500

    Add files via upload
    -fix for input of street (program skips over)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-
-
+//menu, prices, customerAccount, receipt
+        Menu.printMenu();
+        Prices.pricing();
+        CustomerAccount.payment(); //still not print billing address line
+        Receipt.customerName();
+        Receipt.printTime();
 
     }
 }
