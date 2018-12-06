e2e88aafffc6afb211448c1977e128aea11a3b99
is147tix <43656158+is147tix@users.noreply.github.com>
2018-12-05 23:05:45 -0500

    Add files via upload

--- src/com/company/CustomerAccount.java
+++ src/com/company/CustomerAccount.java
@@ -1,40 +1,49 @@
 package com.company;
+
 import java.util.Scanner;
 
-    public class CustomerAccount {
-        static Scanner input = new Scanner(System.in);
-        private int num;
-        private String fullName;
+public class CustomerAccount {
+    static Scanner input = new Scanner(System.in);
+    private int num;
+    private String fullName;
+    private double cardNum;
 
+    public CustomerAccount() {
+    }
 
-    public CustomerAccount(String fullName, int num){
+    public CustomerAccount(String fullName, int num) {
         this.fullName = fullName;
         this.num = num;
     }
 
 
-    public static void payment(){
-        System.out.println("Please Pay using a Credit card (enter 1) or a Debit card (Enter 2).");
+    public void payment() {
+        System.out.println("Please Pay using a Credit card (enter 1) or a Debit card (Enter 2): ");
         int method = input.nextInt();
 
-        System.out.println("Please input your card number");
-        int cardNum = input.nextInt();
+        System.out.println("Please input your card number: ");
+        cardNum = input.nextDouble();
 
-        switch(method){
-            case 1: System.out.println("Please enter zip code");
-                    int zip = input.nextInt();
-                    break;
-            case 2: System.out.println("Please enter pin code");
-                    int pin = input.nextInt();
-                    break;
+
+        switch (method) {
+            case 1:
+                System.out.println("Please enter zip code: ");
+                int zip = input.nextInt();
+                break;
+            case 2:
+                System.out.println("Please enter pin code: ");
+                int pin = input.nextInt();
+                break;
+
         }
 
-                System.out.println("Please input your expiration date for example 08/18 should be entered as 0818.");
-                int expDate = input.nextInt();
-                //if this date is sooner than today, than decline...
+        System.out.print("Please input your expiration date (for example 08/18 should be entered as 0818): ");
+        int expDate = input.nextInt();
 
-                System.out.println("Please input your billing street address. ");
-                String street = input.nextLine();
+        //String card = Double.toString(double cardNum);
+        //String last4char = card.substring(0, 4);
+        //System.out.println("\nCard Number: XXXX XXXX XXXX " + last4char);
+        System.out.println("Has been successfully billed");
 
     }
 
--- src/com/company/Receipt.java
+++ src/com/company/Receipt.java
@@ -1,64 +1,61 @@
 package com.company;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
-    public class Receipt{
+public class Receipt {
     static Scanner input = new Scanner(System.in);
 
+    public static void printTime() {
+        //Get current date time
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm");
+        String formatDateTime = now.format(formatter);
+        System.out.print(formatDateTime + "\n");
+    }
 
-        public static void printTime(){
-            //Get current date time
-            LocalDateTime now = LocalDateTime.now();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            String formatDateTime = now.format(formatter);
-            System.out.print(formatDateTime + "\n");
-        }
+    public static void customerName() {
+        System.out.print("Please enter your first name: ");
+        String enteredFirst = input.nextLine();
+        String firstName = enteredFirst.substring(0, 1).toUpperCase() + enteredFirst.substring(1).toLowerCase();
 
-        public static void customerName() {
-            System.out.print("Please enter your first name: ");
-            String enteredFirst = input.nextLine();
-            String firstName = enteredFirst.substring(0, 1).toUpperCase() + enteredFirst.substring(1).toLowerCase();
+        System.out.print("Please enter your last name: ");
+        String enteredLast = input.nextLine();
+        String lastName = enteredLast.substring(0, 1).toUpperCase() + enteredLast.substring(1).toLowerCase();
 
-            System.out.print("Please enter your last name: ");
-            String enteredLast = input.nextLine();
-            String lastName = enteredLast.substring(0, 1).toUpperCase() + enteredLast.substring(1).toLowerCase();
-
-            int confirmationNum = (int) (Math.random() * 9999 + 1);
-            System.out.println(firstName + " " + lastName + ", your confirmation# is " + confirmationNum);
-            System.out.println("Thank You for your movie purchase, we hope you enjoy the movie!");
-
-
-
-            // CustomerAccount cust1 = new CustomerAccount(fullName, confirmationNum);
-
-        }
-
-
+        int confirmationNum = (int) (Math.random() * 9999 + 1);
+        System.out.println(firstName + " " + lastName + ", your confirmation# is " + confirmationNum);
+        System.out.println("Thank You for your movie purchase, we hope you enjoy the movie!");
 
     }
 
-//private String userName, custFirst, custLast;
 
-       /* public void CustomerInfo(String userFirst, String userLast, int age)
-        {
-            userName = userFirst.concat(" "+userLast);
-            userAge  = age;
-            System.out.println();
-        }
+    private String firstName;
+    private String lastName;
 
-        public void CustomerName(String userFirst, String userLast){
-            custFirst = userFirst;
-            custLast = userLast;
-        }
+    public void Reciept() {
+    }
 
-        public void getConfirmationNum(){
 
-        }
-        public void PrintReceipt(){
+    public void Reciept(String first, String last) {
+        lastName = last;
+        firstName = first;
+    }
 
-            System.out.print("Thank you for purchasing a movie ticket:");
-            System.out.println(userName);
-            System.out.println("Here is your confirmation number: " + confirmationNum);
+    public void setFirstName(String firstName) {
+    }
 
-        }*/
+    public void setLastName(String lastName) {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+}
+
